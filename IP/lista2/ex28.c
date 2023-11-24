a6195e9d85121ee48a0c7c45a6d6c20d8d65a60b
Aust2000 <felipeuberaba10@gmail.com>
2023-11-24 01:01:22 -0300

    lista 2 ex 28 parcial

--- IP/lista2/ex28.c
+++ IP/lista2/ex28.c
@@ -2,10 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-int *uniao(int *vetor1, int *vetor2, int tam1, int tam2) {
-
-}
-
 int max(int a, int b) {
     if (a >= b) {
         return a;
@@ -14,39 +10,41 @@ int max(int a, int b) {
     }
 }
 
-int * intersecao(int *vetor1, int *vetor2, int tam1, int tam2) {
-    int *vet = NULL;
-
-    int resultado = max(tam1, tam2);
-    do{
-        vet = (int *) malloc(resultado * sizeof(int));
-    } while (vet == NULL);
-    
+int uniao(int *vetor1, int *vetor2, int tam1, int tam2, int *resultado) {
+    int i, j, repeated;
     int k = 0;
-    for (int i = 0; i < tam1; i++) {
-        for (int j = 0; j < tam2; j++) {
+    for (i = 0; i < tam1; i++) {
+        resultado[k++] = vetor1[i];
+    }
+
+    for (i = 0; i < tam2; i++) {
+        repeated = 0;
+        for (j = 0; j < tam1; j++) {
+            if (vetor2[i] == vetor1[j]) {
+                repeated = 1;
+                break;
+            }
+        }
+        if (repeated == 0) {
+            resultado[k++] = vetor2[i];
+        }
+    }
+
+    return k;
+}
+
+int intersecao(int *vetor1, int *vetor2, int tam1, int tam2, int *resultado) {    
+    int i, j;
+    int k = 0;
+    for (i = 0; i < tam1; i++) {
+        for (j = 0; j < tam2; j++) {
             if (vetor1[i] == vetor2[j]) {
-                vet[k] = vetor1[i];
-                k++;
+                resultado[k++] = vetor1[i];
             }
         }
     }
 
-    return vet;
-}
-
-//função que le os vetores
-int *lerVetor (int tam) {
-    int vetor[tam];
-    int i;
-    for (i = 0; i < tam; i++) {
-        scanf("%d", &vetor[i]);
-        if (vetor[i] == vetor[i - 1]) {
-            i--;
-        }
-    }
-
-    return vetor;
+    return k;
 }
 
 void printVet(int *vetor, int tam) {
@@ -54,60 +52,55 @@ void printVet(int *vetor, int tam) {
     int i;
     for (i = 0; i < tam; i++) {
         printf("%d", vetor[i]);
-        if (i =! tam - 1) {
+        if (i != tam - 1) {
             printf(",");
         }
     }
     printf(")\n");
 }
 
+//lerVet: lê o vetor já eliminando valores repetidos
+void lerVet(int tam, int *vet) {
+    int i, j;
+    for (i = 0; i < tam; i++) {
+        scanf("%d", &vet[i]);
+        
+        for (j = 0; j < i; j++) {
+            if (vet[i] == vet[j]) {
+                i--;
+            }
+        }
+    }
+}
 
+//lerTam: lê o tamanho até que seja colocado um valor válido e coloca no endereço do parâmetro
+void lerTam(int *tam) {
+    do {
+        scanf("%d", tam);
+    } while (*tam < 1 || *tam > 100);
+}
 
 int main() {
+    int tamUni, tamInter;
     // recebe o tamanho dos vetores
-    int tam1, tam2;
-    while (1) {
-        scanf("%d", &tam1);
-        if (tam1 < 1 || tam1 > 100) {
-            continue;
-        } else {
-            break;
-        }
-    }
-    while (1) {
-        scanf("%d", &tam2);
-        if (tam2 < 1 || tam2 > 100) {
-            continue;
-        } else {
-            break;
-        }
-    }
+    int tam1 = 0;
+    int tam2 = 0;
+    lerTam(&tam1);
+    lerTam(&tam2);
 
     // recebe os vetores, ignorando números repetidos seguidos
     int vetor1[tam1], vetor2[tam2];
-    int i;
-    for (i = 0; i < tam1; i++) {
-        scanf("%d", &vetor1[i]);
-        if (vetor1[i] == vetor1[i - 1]) {
-            i--;
-        }
-    }
-    for (i = 0; i < tam2; i++) {
-        scanf("%d", &vetor2[i]);
-        if (vetor2[i] == vetor2[i - 1]) {
-            i--;
-        }
-    }
+    lerVet(tam1, vetor1);
+    lerVet(tam2, vetor2);
 
     // pega a união e a intersessão dos vetores
-    int vetorUniao[tam1 + tam2];
-    int *vetorInters = NULL;
-    vetorInters = intersecao(vetor1, vetor2, tam1, tam2);
-    
-    printVet(vetorInters, max(tam1, tam2));
-    printVet(uniao, tam1 + tam2);
+    int vetInter[max(tam1, tam2)];
+    tamInter = intersecao(vetor1, vetor2, tam1, tam2, vetInter);
+    int vetUni[tam1 + tam2 - tamInter];
+    tamUni = uniao(vetor1, vetor2, tam1, tam2, vetUni);
 
-    free(vetorInters);
+    printVet(vetUni, tamUni);
+    printVet(vetInter, tamInter);
 
     return 0;
 }
