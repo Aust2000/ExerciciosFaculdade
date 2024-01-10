1432d323e375ba791bfc6d829cd7806cb8c0e6da
Aust2000 <felipeuberaba10@gmail.com>
2024-01-09 23:35:27 -0300

    lista 4 ex 19 inacabado

--- IP/lista4/ex19.c
+++ IP/lista4/ex19.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-void matrixQuadr(int **matriz, int n, double **destino) {
+void copiarMatriz(double **orig, double **dest, int linha, int coluna) {
+    int i, j;
+    for (i = 0; i < linha; i++) {
+        for (j = 0; j < coluna; j++) {
+            dest[i][j] = orig[i][j];
+        }
+    }
+}
+
+void matrizProd(double **matriz1, double **matriz2, int n1, int n2, double **destino) {
+    double resultado[n1][n2];
     int i, j, k;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            for (k = 0; k < n; k++) {
-                destino[i][j] += matriz[i][k] * matriz[k][j];
+    for (i = 0; i < n1; i++) {
+        for (j = 0; j < n2; j++) {
+            resultado[i][j] = 0;
+        }
+    }
+
+    for (i = 0; i < n1; i++) {
+        for (j = 0; j < n2; j++) {
+            for (k = 0; k < n2; k++) {
+                resultado[i][j] += matriz1[i][k] * matriz2[k][j];
             }
         }
     }
+
+    copiarMatriz(resultado, destino, n1, n2);
 }
 
 int main() {
@@ -17,7 +35,7 @@ int main() {
     scanf("%d", &n);
     scanf("%d", &k);
 
-    int matriz[n][n];
+    double matriz[n][n];
     double resultado[n][n];
     int i, j;
     for (i = 0; i < n; i++) {
@@ -28,12 +46,20 @@ int main() {
 
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
-            scanf("%d", &matriz[i][j]);
+            scanf("%lf", &matriz[i][j]);
         }
     }
 
-    for (i = 0; i < k; i++) {
-        matrixQuadr(matriz, n, resultado);
+    matrizProd(matriz, matriz, n, n, resultado);
+    for (i = 1; i < k; i++) {
+        matrizProd(resultado, matriz, n, n, resultado);
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%.2lf", resultado[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
